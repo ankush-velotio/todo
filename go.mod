aaffd5406e6ea92038f479d11a98f8d1717380cf
Ankush Chavan <ankush.chavan@velotio.com>
2021-12-09 16:48:09 +0530

    feat(Auth): Authentication using JWT
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module todo
 
 go 1.17
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/rs/cors v1.8.0
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+)
---
+++ internal/auth/jwt/auth.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtSigningKey = []byte(os.Getenv("SECRET_KEY"))
+
+func GenerateJWT(email string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["email"] = email
+	claims["aud"] = "todo.io"
+	claims["iss"] = "jwtgo.io"
+	claims["exp"] = time.Now().Add(time.Minute * 10).Unix()
+
+	tokenString, err := token.SignedString(jwtSigningKey)
+
+	if err != nil {
+		_ = fmt.Errorf("something Went Wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// IsAuthorized Middleware for verifying the JWT token
+func IsAuthorized(endpoint http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("invalid Signing Method. error in parsing token")
+				}
+				// verify audience claim
+				aud := "todo.io"
+				checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+				if !checkAudience {
+					return nil, fmt.Errorf("invalid audience")
+				}
+				// verify issuer claim
+				iss := "jwtgo.io"
+				checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+				if !checkIss {
+					return nil, fmt.Errorf("invalid issuer")
+				}
+
+				return jwtSigningKey, nil
+			})
+			if err != nil {
+				_, err := fmt.Fprintf(w, err.Error())
+				if err != nil {
+					return
+				}
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			_, err := fmt.Fprintf(w, "No Authorization Token provided")
+			if err != nil {
+				return
+			}
+		}
+	})
+}
