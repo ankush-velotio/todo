7421cce2f83997cdb10f0715bc1152985d008c69
Ankush Chavan <ankush.chavan@velotio.com>
2021-12-11 13:17:55 +0530

    feat(Database): Function for connecting to the PostgreSQL database
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

---
+++ internal/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+	"strconv"
+)
+
+func ConnectDB(port int, dbName, dbUser, dbPassword, host string) *gorm.DB {
+	databaseDialect := "postgres"
+	databaseURL := "postgres://" + dbUser + ":" + dbPassword + "@" + host +":" + strconv.Itoa(port) + "/" + dbName + "?sslmode=disable"
+    connection, err := gorm.Open( databaseDialect, databaseURL)
+
+    if err != nil {
+    	panic("failed to connect database")
+    }
+
+	log.Println("connected to database")
+	return connection
+}
+
+func CloseDB(connection *gorm.DB) error {
+	if err := connection.Close(); err != nil {
+		return errors.New("cannot close current database")
+	}
+	log.Println("database closed successfully")
+	return nil
+}
