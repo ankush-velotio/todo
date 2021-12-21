029b63e2e52cdad8595372807c84c2a915e9e62b
Ankush Chavan <ankush.chavan@velotio.com>
2021-12-21 11:26:13 +0530

    refactor(Router): Router for handling request
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- cmd/todo/todo.go
+++ cmd/todo/todo.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	auth "todo/internal/auth/jwt"
+	"todo/internal/auth/user"
+	"todo/internal/todo"
+)
 
+func authorizeRequest(view http.HandlerFunc) http.Handler {
+	handler, err := auth.IsAuthorized(view)
+	if err != nil {
+		return nil
+	}
+	return handler
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", user.SignUp)
+	router.HandleFunc("/signin", user.SignIn)
+	router.Handle("/todos", authorizeRequest(todo.ListCreateTodoView)).Methods("GET", "POST")
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
